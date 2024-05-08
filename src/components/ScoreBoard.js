faeb72c1159f94f8eb143ce646fac632c0cfaed9
yeunjeong <araruu2536@gmail.com>
2024-05-08 13:35:47 +0900

    score board 수정

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,164 +1,131 @@
 import React from "react";
-import { useTable } from "react-table";
+
+import styles from "./ScoreBoard.module.css";
 
 function ScoreBoard() {
-  const data = React.useMemo(
-    () => [
-      {
-        category: "밭",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "우리",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "곡식",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "채소",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "양",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "멧돼지",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "소",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "빈칸",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "울타리 친 외양간",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "흙집 방",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "돌집 방",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "가족 구성원",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "구걸 카드",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "카드 점수",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "추가 점수",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-      {
-        category: "합계",
-        user1: 0,
-        user2: 0,
-        user3: 0,
-      },
-    ],
-    []
-  );
+  const column = [
+    "",
+    "밭",
+    "우리",
+    "곡식",
+    "채소",
+    "양",
+    "멧돼지",
+    "소",
+    "빈칸",
+    "울타리 친 외양간",
+    "흙집 방",
+    "돌집 방",
+    "가족 구성원",
+    "구걸 카드",
+    "카드 점수",
+    "추가점수",
+  ];
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: " ",
-        accessor: "category",
-      },
-      {
-        Header: "유저1",
-        accessor: "user1",
-      },
-      {
-        Header: "유저2",
-        accessor: "user2",
-      },
-      {
-        Header: "유저3",
-        accessor: "user3",
-      },
-    ],
-    []
-  );
+  const data = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+  const data1 = [
+    {
+      user: "유저1",
+      field: 0,
+      cage: 0,
+      grain: 0,
+      vegetable: 0,
+      sheep: 0,
+      boar: 0,
+      cow: 0,
+      blank: 0,
+      fencedBarn: 0,
+      dirtHouse: 0,
+      stoneHouse: 0,
+      family: 0,
+      beggingCard: 0,
+      cardScore: 0,
+      extraPoints: 0,
+    },
+    {
+      user: "유저2",
+      field: 0,
+      cage: 0,
+      grain: 0,
+      vegetable: 0,
+      sheep: 0,
+      boar: 0,
+      cow: 0,
+      blank: 0,
+      fencedBarn: 0,
+      dirtHouse: 0,
+      stoneHouse: 0,
+      family: 0,
+      beggingCard: 0,
+      cardScore: 0,
+      extraPoints: 0,
+    },
+    {
+      user: "유저3",
+      field: 0,
+      cage: 0,
+      grain: 0,
+      vegetable: 0,
+      sheep: 0,
+      boar: 0,
+      cow: 0,
+      blank: 0,
+      fencedBarn: 0,
+      dirtHouse: 0,
+      stoneHouse: 0,
+      family: 0,
+      beggingCard: 0,
+      cardScore: 0,
+      extraPoints: 0,
+    },
+    {
+      user: "유저4",
+      field: 0,
+      cage: 0,
+      grain: 0,
+      vegetable: 0,
+      sheep: 0,
+      boar: 0,
+      cow: 0,
+      blank: 0,
+      fencedBarn: 0,
+      dirtHouse: 0,
+      stoneHouse: 0,
+      family: 0,
+      beggingCard: 0,
+      cardScore: 0,
+      extraPoints: 0,
+    },
+  ];
 
   return (
-    <table
-      {...getTableProps()}
-      style={{ height: "10%", borderCollapse: "collapse", fontSize: "10%" }}
-    >
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr key={i} {...row.getRowProps()} style={{ height: "5%" }}>
-              {row.cells.map((cell) => (
-                <td
-                  key={cell.getCellProps().key}
-                  {...cell.getCellProps()}
-                  style={{
-                    width: cell.column.width || "auto",
-                    height: "5%",
-                    border: "1px solid #000",
-                    padding: "8px",
-                    textAlign: "center",
-                  }}
-                >
-                  {cell.render("Cell")}
-                </td>
+    <div className={styles.container}>
+      점수 현황
+      <div className={styles.box}>
+        <div className={styles.box1}>
+          {column.map((data, index) => (
+            <div className={styles.box11}>{data}</div>
+          ))}
+          <div className={styles.box12}>합계</div>
+        </div>
+        {[...Array(4)].map((_, index) => (
+          <div className={styles.box2}>
+            {data[index].user}
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              {data.map((_, index) => (
+                <div>dd</div>
               ))}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
