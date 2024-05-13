6aafa8391ea243ff9a33bf9e273a55114ad30441
yeunjeong <araruu2536@gmail.com>
2024-05-13 13:35:34 +0900

    lobby 약간 수정

--- src/routes/Lobby.js
+++ src/routes/Lobby.js
@@ -6,10 +6,17 @@ import styles from "./Lobby.module.css";
 function Lobby() {
   return (
     <div className={styles.container}>
-      <div></div>
-      <Link to="/main">
-        <li>rodla</li>
-      </Link>
+      <div className={styles.header}>AGRICOLA</div>
+      <div className={styles.body}>
+        <div className={styles.roomlist}>
+          <div className={styles.list}></div>
+        </div>
+        <div className={styles.startbtn}>
+          <Link to="/main" className={styles.button}>
+            <div>방만들기</div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
