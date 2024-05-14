87d2b9cb4773d492896b6b6456da8572363bbef8
yeunjeong <araruu2536@gmail.com>
2024-05-14 18:55:32 +0900

    헤더 메인으로 병합

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,17 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+
 import styles from "./Main.module.css";
 
-import Header from "../components/Header";
 import ActBoard from "../components/ActBoard";
 import HomeBoard from "../components/HomeBoard";
 import CardBoard from "../components/CardBoard";
 import LogBoard from "../components/LogBoard";
 
 function Main() {
+  const [roomnumber, setRoomnumber] = useState();
+
+  // 컴포넌트가 마운트될 때 쿠키에서 방 번호를 가져온다.
+  useEffect(() => {
+    const savedRoomNumber = Cookies.get("roomnumber");
+    if (savedRoomNumber) {
+      setRoomnumber(savedRoomNumber);
+    }
+  }, []);
+
+  // 방 이름을 출력한다.
+  useEffect(() => {
+    if (roomnumber) {
+      console.log(roomnumber);
+    }
+  }, [roomnumber]);
+
   return (
     <div className={styles.container}>
       <div className={styles.leftBoard}>
         <div className={styles.header}>
-          <Header />
+          <div className={styles.text}>ROOM #{roomnumber}</div>
         </div>
         <div className={styles.gameBoard}>
           <div className={styles.actBoard}>
